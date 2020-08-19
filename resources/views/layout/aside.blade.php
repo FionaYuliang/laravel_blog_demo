aadc854d40c1e61cb889d360658b9911b4081b39
yangmingming <yangmingming94@gmail.com>
2020-08-19 18:13:32 +0800

    首页样式细化

--- resources/views/layout/aside.blade.php
+++ resources/views/layout/aside.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 mb-3 bg-light rounded">
+<div class="p-4 mb-3 bg-light rounded" style="margin-top: 20px">
     <h4 class="font-italic">About</h4>
     <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
 </div>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,22 +1,21 @@
 @extends("layout.main")
 @section("content")
-    <div class="blog-main">
+    <div class="blog-main" style="margin-top: 20px">
 @include("layout.errors")
-
-        <form action="/posts/index/store" method="POST">
+    <form action="/posts/index/store" method="POST">
 {{--            <input type="hidden" name="_token" value="{{csrf_token()}}">--}}
-            {{csrf_field()}}
-            <div class="form-group">
-                <label>标题</label>
-                <input name="title" type="text" class="form-control" placeholder="这里是标题">
-            </div>
-            <div class="form-group">
-                <label>内容</label>
-                <textarea id="content" name="content" class="form-control" style="height:400px;max-height:500px;"  placeholder="请输入文章内容">
-                </textarea>
-            </div>
-            <button type="submit" class="btn btn-default">提交</button>
-        </form>
+        {{csrf_field()}}
+        <div class="form-group">
+            <label>标题</label>
+            <input name="title" type="text" class="form-control" placeholder="这里是标题">
+        </div>
+        <div class="form-group">
+            <label>内容</label>
+            <textarea id="content" name="content" class="form-control" style="height:400px;max-height:500px;"  placeholder="请输入文章内容">
+            </textarea>
+        </div>
+        <button type="submit" class="btn btn-info">提交</button>
+    </form>
         <br>
     </div><!-- /.blog-main -->
 @endsection
