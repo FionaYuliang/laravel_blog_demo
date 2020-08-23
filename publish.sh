159b9a4773ca525e7be15b58c48a4a1077a65f23
yangmingming <yangmingming94@gmail.com>
2020-08-24 00:26:26 +0800

    feat: 优化部署脚本

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
-su wordpress
+#   su wordpress
+#   bash ~/publish.sh
+
 cd /home/wordpress/www/laravel_blog_demo
 #  直接拉取远程代码
 git fetch --all
 git reset --hard origin/master
 #   安装composer依赖
-composer install
+composer install -n
